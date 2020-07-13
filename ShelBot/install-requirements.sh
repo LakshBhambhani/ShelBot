c1d6b439e16e8fb362a7864be98c1bb56e48daff
Laksh Bhambhani <lakshbh@gmail.com>
2020-07-13 13:55:08 -0700

    added speech recognition

---
+++ ShelBot/install-requirements.sh
@@ -0,0 +1,17 @@
+pip3 install glob2
+pip3 install ipython
+pip3 install keras
+pip3 install librosa
+pip3 install matplotlib
+pip3 install numpy
+pip3 install pandas
+pip3 install plotly
+pip3 install scipy
+pip3 install seaborn
+pip3 install sklearn
+pip3 install tensorflow
+pip3 install tqdm
+pip3 install numba==0.48
+pip3 install pickle
+pip3 install sounddevice
+pip3 install soundfile
