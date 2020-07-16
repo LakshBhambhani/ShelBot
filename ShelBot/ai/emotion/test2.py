3640af064c92c3e13527007c7e3e6d7d3f73e7d7
Laksh Bhambhani <lakshbh@gmail.com>
2020-07-15 18:46:34 -0700

    Update test2.py

--- ShelBot/ai/emotion/test2.py
+++ ShelBot/ai/emotion/test2.py
@@ -48,55 +48,54 @@ def plot_model_history(model_history):
     fig.savefig('plot.png')
     plt.show()
 
+# Define data generators
+train_dir = "../RawData/facial-expression/images/train/"
+val_dir = "../RawData/facial-expression/images/validation/"
+
+num_train = 28709
+num_val = 7178
+batch_size = 64
+num_epoch = 50
+
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
+
+# Create the model
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
 
 # If you want to train the same model or try other models, go for this
 if(retrain):
-
-    # Define data generators
-    train_dir = "../RawData/facial-expression/images/train/"
-    val_dir = "../RawData/facial-expression/images/validation/"
-
-    num_train = 28709
-    num_val = 7178
-    batch_size = 64
-    num_epoch = 50
-
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    val_datagen = ImageDataGenerator(rescale=1./255)
-
-    train_generator = train_datagen.flow_from_directory(
-            train_dir,
-            target_size=(48,48),
-            batch_size=batch_size,
-            color_mode="grayscale",
-            class_mode='categorical')
-
-    validation_generator = val_datagen.flow_from_directory(
-            val_dir,
-            target_size=(48,48),
-            batch_size=batch_size,
-            color_mode="grayscale",
-            class_mode='categorical')
-
-    # Create the model
-    model = Sequential()
-
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))
-
     model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
     model_info = model.fit_generator(
             train_generator,
