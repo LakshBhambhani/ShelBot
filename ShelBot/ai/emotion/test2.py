2a824020a25fabf843bcc0a121f6b498079c38d0
Laksh Bhambhani <lakshbh@gmail.com>
2020-07-15 18:44:31 -0700

    Update test2.py

--- ShelBot/ai/emotion/test2.py
+++ ShelBot/ai/emotion/test2.py
@@ -1,3 +1,5 @@
+print("[*] Importing packages...")
+
 import numpy as np
 import argparse
 import matplotlib.pyplot as plt
@@ -11,10 +13,14 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode",help="train/display")
-mode = ap.parse_args().mode
+debug = False
+retrain = False
+
+for args in sys.argv[1:]:
+    if(args == "-r" or args == "--retrain"):
+        retrain = True
+    elif(args == "-d" or args == "--debug"):
+        debug = True
 
 # plots accuracy and loss curves
 def plot_model_history(model_history):
@@ -41,53 +47,55 @@ def plot_model_history(model_history):
     fig.savefig('plot.png')
     plt.show()
 
-# Define data generators
-train_dir = "../RawData/facial-expression/images/train/"
-val_dir = "../RawData/facial-expression/images/validation/"
-
-num_train = 28709
-num_val = 7178
-batch_size = 64
-num_epoch = 50
-
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
-
-train_generator = train_datagen.flow_from_directory(
-        train_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-validation_generator = val_datagen.flow_from_directory(
-        val_dir,
-        target_size=(48,48),
-        batch_size=batch_size,
-        color_mode="grayscale",
-        class_mode='categorical')
-
-# Create the model
-model = Sequential()
-
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(7, activation='softmax'))
 
 # If you want to train the same model or try other models, go for this
-if mode == "train":
+if(retrain):
+
+    # Define data generators
+    train_dir = "../RawData/facial-expression/images/train/"
+    val_dir = "../RawData/facial-expression/images/validation/"
+
+    num_train = 28709
+    num_val = 7178
+    batch_size = 64
+    num_epoch = 50
+
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    val_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=(48,48),
+            batch_size=batch_size,
+            color_mode="grayscale",
+            class_mode='categorical')
+
+    validation_generator = val_datagen.flow_from_directory(
+            val_dir,
+            target_size=(48,48),
+            batch_size=batch_size,
+            color_mode="grayscale",
+            class_mode='categorical')
+
+    # Create the model
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
+
     model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
     model_info = model.fit_generator(
             train_generator,
@@ -99,7 +107,7 @@ if mode == "train":
     model.save_weights('model.h5')
 
 # emotions will be displayed on your face from the webcam feed
-elif mode == "display":
+else:
     print("[*] Loading the model...")
     model.load_weights('model.h5')
 
@@ -120,7 +128,6 @@ elif mode == "display":
             print("[*] Camera not found")
             break
 
-        print("[*] Detecting faces...")
         facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
@@ -135,7 +142,8 @@ elif mode == "display":
             prediction = model.predict(cropped_img)
             maxindex = int(np.argmax(prediction))
             cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-            print(emotion_dict[maxindex], prediction[maxindex])
+            if(debug):
+                print(emotion_dict[maxindex])
 
         # cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
         if cv2.waitKey(1) & 0xFF == ord('q'):
