130342587004151c5a5bf154b524e79a2892f0f8
Laksh Bhambhani <lakshbh@gmail.com>
2020-11-27 21:43:57 -0800

    commiting remaining stuff

--- ShelBot/ai/emotion/facial-expression.py
+++ ShelBot/ai/emotion/facial-expression.py
@@ -1,26 +1,18 @@
 print("[*] Importing packages...")
 
 import numpy as np
-import seaborn as sns
+import argparse
 import matplotlib.pyplot as plt
+import cv2
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 import sys
-import cv2
-
-# Keras
-import tensorflow
-from tensorflow import keras
-tensorflow.__version__
-
-from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
-from tensorflow.keras.layers import Dense, Input, Dropout, GlobalAveragePooling2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras.models import model_from_json
-
-from sklearn.neural_network import MLPClassifier # multi-layer perceptron model
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 debug = False
 retrain = False
@@ -31,182 +23,131 @@ for args in sys.argv[1:]:
     elif(args == "-d" or args == "--debug"):
         debug = True
 
-epochs = 50
+# plots accuracy and loss curves
+def plot_model_history(model_history):
+    """
+    Plot Accuracy and Loss curves given the model_history
+    """
+    fig, axs = plt.subplots(1,2,figsize=(15,5))
+    # summarize history for accuracy
+    axs[0].plot(range(1,len(model_history.history['accuracy'])+1),model_history.history['accuracy'])
+    axs[0].plot(range(1,len(model_history.history['val_accuracy'])+1),model_history.history['val_accuracy'])
+    axs[0].set_title('Model Accuracy')
+    axs[0].set_ylabel('Accuracy')
+    axs[0].set_xlabel('Epoch')
+    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+    axs[0].legend(['train', 'val'], loc='best')
+    # summarize history for loss
+    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+    axs[1].set_title('Model Loss')
+    axs[1].set_ylabel('Loss')
+    axs[1].set_xlabel('Epoch')
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+    axs[1].legend(['train', 'val'], loc='best')
+    fig.savefig('plot.png')
+    plt.show()
 
-mjson_file = "model.json"
-mweights_file = "model_weights.h5"
+# Define data generators
+train_dir = "../RawData/facial-expression/images/train/"
+val_dir = "../RawData/facial-expression/images/validation/"
 
-# size of the image: 48*48 pixels
-pic_size = 48
+num_train = 28709
+num_val = 7178
+batch_size = 64
+num_epoch = 50
 
-# input path for the images
-base_path = "../RawData/facial-expression/images/"
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
 
-EMOTIONS_LIST = ["Angry", "Happy",
-                    "Disgust", "Fear",
-                     "Neutral", "Sad",
-                     "Surprise"]
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
 
-plt.figure(0, figsize=(12,20))
-cpt = 0
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=batch_size,
+        color_mode="grayscale",
+        class_mode='categorical')
 
-def load_model(model_json_file, model_weights_file):
-    # load model from JSON file
-    with open(model_json_file, "r") as json_file:
-        loaded_model_json = json_file.read()
-        loaded_model = model_from_json(loaded_model_json)
+# Create the model
+model = Sequential()
 
-    # load weights into the new model
-    loaded_model.load_weights(model_weights_file)
-    return loaded_model
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-def predict_emotion(loaded_model, img):
-    preds = loaded_model.predict(img)
-    # print("PREDS:  ", preds)
-    return EMOTIONS_LIST[np.argmax(preds)]
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
 
 
+# If you want to train the same model or try other models, go for this
 if(retrain):
+    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+    model_info = model.fit_generator(
+            train_generator,
+            steps_per_epoch=num_train // batch_size,
+            epochs=num_epoch,
+            validation_data=validation_generator,
+            validation_steps=num_val // batch_size)
+    plot_model_history(model_info)
+    model.save_weights('model.h5')
 
-    if(debug):
-        for expression in os.listdir(base_path + "train/"):
-            for i in range(1,6):
-                cpt = cpt + 1
-                plt.subplot(7,5,cpt)
-                img = load_img(base_path + "train/" + expression + "/" +os.listdir(base_path + "train/" + expression+"/")[i], target_size=(pic_size, pic_size))            
-                plt.imshow(img, cmap="gray")
-
-        plt.tight_layout()
-        plt.show()
-
-    if(debug):
-        for expression in os.listdir(base_path + "train"):
-            print(str(len(os.listdir(base_path + "train/" + expression))) + " " + expression + " images")
-
-    # number of images to feed into the NN for every batch
-    print("[*] Training the model...")
-    batch_size = 128
-
-    datagen_train = ImageDataGenerator()
-    datagen_validation = ImageDataGenerator()
-
-    train_generator = datagen_train.flow_from_directory(base_path + "train",
-                                                        target_size=(pic_size,pic_size),
-                                                        color_mode="grayscale",
-                                                        batch_size=batch_size,
-                                                        class_mode='categorical',
-                                                        shuffle=True)
-
-    validation_generator = datagen_validation.flow_from_directory(base_path + "validation",
-                                                        target_size=(pic_size,pic_size),
-                                                        color_mode="grayscale",
-                                                        batch_size=batch_size,
-                                                        class_mode='categorical',
-                                                        shuffle=False)
-
-    # number of possible label values
-    nb_classes = 7
-
-    # Initialising the CNN
-    model = Sequential()
-
-    # 1 - Convolution
-    model.add(Conv2D(64,(3,3), padding='same', input_shape=(48, 48,1)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # 2nd Convolution layer
-    model.add(Conv2D(128,(5,5), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # 3rd Convolution layer
-    model.add(Conv2D(512,(3,3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # 4th Convolution layer
-    model.add(Conv2D(512,(3,3), padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    # Flattening
-    model.add(Flatten())
-
-    # Fully connected layer 1st layer
-    model.add(Dense(256))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-
-    # Fully connected layer 2nd layer
-    model.add(Dense(512))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-
-    model.add(Dense(nb_classes, activation='softmax'))
-
-    opt = Adam(lr=0.0001)
-    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
-    checkpoint = ModelCheckpoint("model_weights.h5", monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    callbacks_list = [checkpoint]
-
-    history = model.fit_generator(generator=train_generator,
-                                    steps_per_epoch=100,#train_generator.n//train_generator.batch_size,
-                                    epochs=epochs,
-                                    validation_data = validation_generator,
-                                    validation_steps = 2,#validation_generator.n//validation_generator.batch_size,
-                                    callbacks=callbacks_list
-                                    )
+# emotions will be displayed on your face from the webcam feed
 else:
     print("[*] Loading the model...")
-    # loaded_model = load_model(mjson_file, mweights_file)
-    knn = 
-    
-    if(debug):
-        loaded_model.summary()
-    
-    facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    model.load_weights('model.h5')
 
-    video = cv2.VideoCapture(0)
+    # prevents openCL usage and unnecessary logging messages
+    cv2.ocl.setUseOpenCL(False)
 
-    print("[*] Running recog")
+    # dictionary which assigns each label an emotion (alphabetical order)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    # start the webcam feed
+    print("[*] Loading camera...")
+    cap = cv2.VideoCapture(0)
     while True:
-        _, fr = video.read()
-        fr = cv2.rotate(fr, cv2.ROTATE_180)
-        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-        faces = facec.detectMultiScale(gray_fr, 1.5, 4)
+        # Find haar cascade to draw bounding box around face
+        ret, frame = cap.read()
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+        if not ret:
+            print("[*] Camera not found")
+            break
 
-        print("FAces: ", faces)
+        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
-        # final_gray_fr = cv2.resize(gray_fr, (48, 48))
-        # pre = predict_emotion(loaded_model, final_gray_fr[np.newaxis, :, :, np.newaxis])
-        # print(pre)
+        cv2.imwrite("original_fr.png", frame)
+        cv2.imwrite("grayscale_fr.png", gray)
 
         for (x, y, w, h) in faces:
-            fc = gray_fr[y:y+h, x:x+w]
-
-            roi = cv2.resize(fc, (48, 48))
-            pred = predict_emotion(loaded_model, roi[np.newaxis, :, :, np.newaxis])
-            print(pred)
-
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(frame, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
             if(debug):
-                cv2.rectangle(fr, (x,y), (x+w, y+h), (255, 0, 0), 3)
+                print(emotion_dict[maxindex])
 
-        if(debug):
-            cv2.imwrite("original_fr.png", fr)
-            cv2.imwrite("grayscale_fr.png", gray_fr)
+        # cv2.imshow('Video', cv2.resize(frame,(1600,960),interpolation = cv2.INTER_CUBIC))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-
-
-
+    cap.release()
+    cv2.destroyAllWindows()
